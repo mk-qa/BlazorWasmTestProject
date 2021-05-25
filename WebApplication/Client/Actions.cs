9280f5b68297e42b0a6fca81a5a9cec6bbf2e7df
mk <marianna.kurtukova@jetbrains.com>
2021-05-25 18:45:00 +0300

    create app

---
+++ WebApplication/Client/Actions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebApplication.Client
+{
+    public class Actions
+    {
+        public int GenerateRandomNumber()
+        {
+            Random n = new Random();
+            return n.Next();
+        }
+
+        public string GetText()
+        {
+            return "This is TEXT";
+        }
+
+
+    }
+}
